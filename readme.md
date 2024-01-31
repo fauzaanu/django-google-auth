5b0da67e7c8f895d02ea5541c7d3a0e70d20c108
fauzaanu <fauzaangasim@gmail.com>
2024-01-31 14:50:06 +0500

    remove pyc

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Simpler google auth for django apps
+
+Full credit to [photondesigner](https://www.photondesigner.com/articles/google-sign-in?ref=yt-google-sign-in) as this is exactly what he implemented in his youtube video and guide _**with minimal changes**_
+
+### Changes brought
+- Not hard coding the client id in html
+- Seperated the copy-paste html section to another file
+
+### To do
+- More providers
+- Cover different examples of saving user data to database
+
+### Credits
+- https://www.youtube.com/watch?v=NM9BE0iUB5Q
+- https://www.photondesigner.com/articles/google-sign-in?ref=yt-google-sign-in
--- signin/settings.py
+++ signin/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'googleauth.apps.GoogleauthConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,8 @@ ROOT_URLCONF = 'signin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
